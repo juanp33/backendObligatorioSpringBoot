e8d56a54bf7c93e0a6c9d277d66077c1fd2fdf92
Juan <33juanpintos33@gmail.com>
2024-10-24 15:40:33 -0300

    Agregada seguridad CORS

--- src/main/java/com/example/demo/Modelos/APIChatGPT.java
+++ src/main/java/com/example/demo/Modelos/APIChatGPT.java
@@ -1,9 +1,6 @@
 package com.example.demo.Modelos;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.util.List;
 @Entity
@@ -11,5 +8,6 @@ public class APIChatGPT {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @OneToMany
     private List<Pregunta> preguntas;
 }
