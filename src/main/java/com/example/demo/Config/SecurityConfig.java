e10fff6ef8ca76dc07c6b87bfa61a48d19c884e5
Juan <33juanpintos33@gmail.com>
2024-10-24 20:18:35 -0300

    Cambio logica api

--- src/main/java/com/example/demo/Config/SecurityConfig.java
+++ src/main/java/com/example/demo/Config/SecurityConfig.java
@@ -6,11 +6,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     private final UsuarioDetailsService usuarioDetailsService;
@@ -23,18 +24,26 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeHttpRequests()
-                .requestMatchers("/api/auth/register", "/api/auth/login").permitAll() // Permitir acceso a estas rutas sin autenticación
-                .anyRequest().authenticated() // Requerir autenticación para todas las demás rutas
-                .and()
-                .formLogin() // Habilitar autenticación basada en formularios, lo que gestiona automáticamente las sesiones
-                .and()
-                .logout().permitAll(); // Permitir a los usuarios cerrar sesión
+        http.csrf(csrf -> csrf.disable()) // Deshabilitar CSRF para aplicaciones REST, opcional
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/register", "/api/auth/login").permitAll() // Permitir estas rutas sin autenticación
+                        .anyRequest().authenticated() // Todas las demás rutas requieren autenticación
+                )
+                .formLogin(form -> form
+                        .loginProcessingUrl("/api/auth/login") // Indica a Spring que maneje la autenticación en esta URL
+                        .permitAll() // Permitir acceso al login
+                )
+                .logout(logout -> logout.permitAll()); // Permitir a los usuarios cerrar sesión
 
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        authBuilder.userDetailsService(usuarioDetailsService).passwordEncoder(passwordEncoder());
+        return authBuilder.build();
     }
 }
