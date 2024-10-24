9d6face4e9f6266c90c3d083a2a3f0262f27ee1d
Juan <33juanpintos33@gmail.com>
2024-10-24 15:22:21 -0300

    Agregada seguridad CORS

--- src/main/java/com/example/demo/Modelos/JugarConBot.java
+++ src/main/java/com/example/demo/Modelos/JugarConBot.java
@@ -1,9 +1,15 @@
 package com.example.demo.Modelos;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class JugarConBot extends Juego implements Ganador {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String nivelDificultad; // Valores posibles: "facil", "medio", "dificil"
     private int puntajeJugador;
     private int puntajeBot;
--- src/main/java/com/example/demo/Modelos/SinglePlayer.java
+++ src/main/java/com/example/demo/Modelos/SinglePlayer.java
@@ -1,9 +1,15 @@
 package com.example.demo.Modelos;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class SinglePlayer extends Juego {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private int puntaje;
     private int tiempo;
 }
